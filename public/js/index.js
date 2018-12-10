d31944faa5bdb2e69e906e9a95846f9a5879b756
kshay <kshayk0@gmail.com>
2018-12-10 12:28:37 +0200

    Adding a form and messages section in the UI

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,6 @@ var socket = io();
 //Not using arrow functions in frontend because a lot of devices has no support for ES6
 socket.on('connect', function() {
     console.log('hey');
-
-    socket.emit('createMessage', {
-        from: 'shay',
-        text: 'test text'
-    }, function(message) {
-        console.log('sent', message);
-    });
 });
 
 socket.on('disconnect', function() {
@@ -17,5 +10,16 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log(message);
+    $("#messages").append(`<li>From: ${message.from} | text: ${message.text}</li>`);
+});
+
+$('#messageForm').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'anon',
+        text: $("[name=message]").val()
+    }, function(message) {
+        console.log('sent', message);
+    });
 });
